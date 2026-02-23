c93ea13f061b5e56a584859b5a6b43482e1da7f4
Abdoubba5 <158740943+Abdoubba5@users.noreply.github.com>
2026-02-23 01:56:46 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* Counter Animation */
+
+const counters = document.querySelectorAll(".counter")
+
+counters.forEach(counter=>{
+
+counter.innerText="0"
+
+const update=()=>{
+
+const target = +counter.getAttribute("data-target")
+
+const value = +counter.innerText
+
+const increment = target / 100
+
+if(value < target){
+
+counter.innerText = Math.ceil(value + increment)
+
+setTimeout(update,30)
+
+}else{
+
+counter.innerText = target
+
+}
+
+}
+
+update()
+
+})
