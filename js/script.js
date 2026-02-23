d8de71be47a85c0630b792e9a8ee71443d6d90dc
Abdoubba5 <158740943+Abdoubba5@users.noreply.github.com>
2026-02-23 02:08:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,3 +29,22 @@ counter.innerText = target;
 update();
 
 });
+
+
+const faders = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll",()=>{
+
+faders.forEach(el=>{
+
+const top = el.getBoundingClientRect().top;
+
+if(top < window.innerHeight-100){
+
+el.classList.add("show");
+
+}
+
+});
+
+});
